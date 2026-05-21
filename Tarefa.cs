5cfdf849161c19eb7a5bcd06e0820cd5caf67650
MarceloCaires438 <kailor458@gmail.com>
2026-05-21 19:03:22 -0300

    Atualizar o Tarefa.cs

--- Tarefa.cs
+++ Tarefa.cs
@@ -5,7 +5,7 @@ namespace PROJETO_POO_CRUD.Models
     public class Tarefa
     {
         public int Id { get; set; }
-        public string TextoTarefa { get; set; } // Mudamos o nome aqui para não dar conflito!
+        public string TextoTarefa { get; set; }
         public string? Descricao { get; set; }
         public DateTime DataCriacao { get; set; }
         public bool Concluida { get; set; }
